d84c9ffe9b8334a7ea19e50e98690b93c130a404
aviatam-mend <149406890+aviatam-mend@users.noreply.github.com>
2024-10-30 12:32:24 +0200

    Update BasicTest.java

--- BasicTest.java
+++ BasicTest.java
@@ -1,21 +1,12 @@
-package a.b.c;
 
-import javax.servlet.http.*;
+public class VulnerableActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vulnerable);
 
-public class BasicTest extends HttpServlet {
-  
-  @Override
-  public void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws IOException {
-    final var param = req.getParameter("param");
-    // This makes the output unsafe.
-    resp.setHeader("content-type", "text/html");
-    var param1 = cleanParam(param);
-    int x = 2*2;
-    resp.getWriter().println(param.repeat(x));    
-    resp.getWriter().println(param1.repeat(x));
-  }
-
-
-  public String cleanParam(String x){ return "";}
-
+        String param = getIntent().getStringExtra("param");
+        String query = "SELECT * FROM users WHERE username = '" + param + "'";
+        // Execute the query using your database logic, e.g., SQLite
+    }
 }
